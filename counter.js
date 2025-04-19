e637517fa4079daf45b1aa8f5678912cc64ec377
louis123472 <holonlouismurindankaka@gmail.com>
2025-04-19 20:32:37 +0200

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,10 @@
+let counter = 0;
+
+function count(){ counter++;
+    document.querySelector('h1').innerHTML = counter;
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    document.querySelector('button').onclick = count;
+    setInterval(count,1000);
+})
